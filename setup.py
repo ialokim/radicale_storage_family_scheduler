f5665995158b58e02f95dc091033e75909b5c4a8
Ialokim <ialokim@t-online.de>
2018-03-22 20:23:19 +0100

    updated setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
-setup(name="radicale_storage_family_scheduler", packages=["radicale_storage_family_scheduler"])
+setup(
+    name="radicale_storage_family_scheduler",
+    version='0.0.1',
+    description='Basic scheduling for users on the same server',
+    url='https://github.com/ialokim/radicale_storage_family_scheduler',
+    license='CC-BY-SA-4.0',
+    keywords='radicale storage scheduling caldav python',
+    author='ialokim',
+
+    install_requires=['radicale'],
+    packages=["radicale_storage_family_scheduler"]
+)
